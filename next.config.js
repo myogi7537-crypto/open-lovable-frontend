2b545e3c11b4848dc572ee4077a892a7df95542f
myogi7537-crypto <myogi7537@gmail.com>
2026-01-13 11:31:21 +0530

    fix: remove next/font errors, use Google Fonts CDN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // turbo या wasm bindings हटाओ
+  },
 };
 
 module.exports = nextConfig;
